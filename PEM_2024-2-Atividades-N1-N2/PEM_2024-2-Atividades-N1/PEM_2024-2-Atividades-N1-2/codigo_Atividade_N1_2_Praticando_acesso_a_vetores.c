4089954cc4a0b563206142640bc3f8a25585c54e
Stephanie Carolline Amarante <89923920+SteAmarante@users.noreply.github.com>
2024-08-23 15:59:34 -0300

    Update codigo_Atividade_N1_2_Praticando_acesso_a_vetores.c

--- PEM_2024-2-Atividades-N1-N2/PEM_2024-2-Atividades-N1/PEM_2024-2-Atividades-N1-2/codigo_Atividade_N1_2_Praticando_acesso_a_vetores.c
+++ PEM_2024-2-Atividades-N1-N2/PEM_2024-2-Atividades-N1/PEM_2024-2-Atividades-N1-2/codigo_Atividade_N1_2_Praticando_acesso_a_vetores.c
@@ -1,13 +1,15 @@
 //Stephanie Carolline Amarante - RA2040482222049
+// Stephanie Carolline Amarante - RA2040482222049
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAX_NOTAS 5
 #define MAX_PROVAS 3
 
 // Função para calcular a soma das notas
-float soma_notas(float notas[]) {
+float soma_notas(float notas[], int num_notas) {
     float soma = 0.0;
-    for (int i = 0; i < MAX_NOTAS; i++) {
+    for (int i = 0; i < num_notas; i++) {
         soma += notas[i];
     }
     return soma;
@@ -20,11 +22,21 @@ int main() {
     // Lê as notas de cada prova
     for (int i = 0; i < MAX_PROVAS; i++) {
         printf("Prova %d:\n", i + 1);
+        printf("Digite as %d notas, separadas por espaços: ", MAX_NOTAS);
+
+        // Lê a linha inteira de notas
         for (int j = 0; j < MAX_NOTAS; j++) {
-            printf("Nota %d: ", j + 1);
-            scanf("%f", &notas[j]);
+            if (scanf("%f", &notas[j]) != 1) {
+                fprintf(stderr, "Entrada inválida. Por favor, insira números válidos.\n");
+                exit(EXIT_FAILURE);
+            }
         }
-        provas[i] = soma_notas(notas);
+
+        // Limpa o buffer de entrada após ler todas as notas
+        while (getchar() != '\n');
+
+        // Calcula a soma das notas
+        provas[i] = soma_notas(notas, MAX_NOTAS);
         printf("Soma das notas: %.2f\n", provas[i]);
     }
 
